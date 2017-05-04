eb504634a65359e70961f1b6cc854650ae059491
willcodes <difruscio.dev@gmail.com>
2017-05-03 23:51:42 -0400

    init

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+int main()
+{
+    VideoMode vm(800,600);
+    RenderWindow window(vm,"Game Boilerplate", Style::Titlebar);
+
+    Texture textureBackground;
+    textureBackground.loadFromFile("assets/purple.png");
+    
+    Sprite spriteBackground;
+    spriteBackground.setTexture(textureBackground);
+    spriteBackground.setPosition(0,0);
+    spriteBackground.setScale(3.125,2.34);
+
+    while (window.isOpen())
+    {
+        if (Keyboard::isKeyPressed(Keyboard::Escape))
+        {
+            window.close();     
+        }
+        window.clear();
+        window.draw(spriteBackground);
+        window.display();
+    }
+    return 0;
+}
