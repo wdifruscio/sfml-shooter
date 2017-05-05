314ca38b91ffc47bb5b6b2b6e9548cec52e77896
willcodes <difruscio.dev@gmail.com>
2017-05-04 23:51:13 -0400

    added basic input

--- compile.sh
+++ compile.sh
@@ -3,12 +3,8 @@
 echo "Compiling to source and linking headers";
 g++ -c main.cpp;
 
-
 g++ main.o -o sfml-game -lsfml-graphics -lsfml-window -lsfml-system;
 
-
 echo "Done!"
-
-
 
 exit 0;
--- main.cpp
+++ main.cpp
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <vector>
 
 using namespace sf;
 
 int main()
 {
-    VideoMode vm(800,600);
-    RenderWindow window(vm,"Game Boilerplate", Style::Titlebar);
+    VideoMode vm(1920,1080);
+    RenderWindow window(vm,"Game Boilerplate");
+    window.setFramerateLimit(60);
+
 
     Texture textureBackground;
     textureBackground.loadFromFile("assets/purple.png");
-    
+    textureBackground.setRepeated(true);
+
     Sprite spriteBackground;
     spriteBackground.setTexture(textureBackground);
-    spriteBackground.setPosition(0,0);
-    spriteBackground.setScale(3.125,2.34);
-
+    spriteBackground.setTextureRect(IntRect(0, 0, 1920, 1080));
+    
     Texture textureShip;
     textureShip.loadFromFile("assets/player.png");
 
     Sprite playerShip;
     playerShip.setTexture(textureShip);
-    playerShip.getLocalBounds();
-
-
+    playerShip.setPosition(1920 / 2.0f , 900);
+    int playerShipX = playerShip.getPosition().x;
+    int playerShipY = playerShip.getPosition().y;
+    float playerSpeed = 1.5;
 
     while (window.isOpen())
     {
@@ -32,8 +36,23 @@ int main()
         {
             window.close();     
         }
+        if (Keyboard::isKeyPressed(Keyboard::Left))
+        {
+            playerShipX += -1 * playerSpeed;
+            playerShip.setPosition(playerShipX, playerShipY);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Right))
+        {
+            playerShipX += 1 * playerSpeed;
+            playerShip.setPosition(playerShipX, playerShipY);
+        }
+
         window.clear();
+
+        //rendering
         window.draw(spriteBackground);
+        window.draw(playerShip);
+
         window.display();
     }
     return 0;
