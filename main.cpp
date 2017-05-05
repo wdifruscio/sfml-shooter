83e895762c7a609d9284ef39659325c38c0ca619
willcodes <difruscio.dev@gmail.com>
2017-05-05 07:49:15 -0400

    added player controller and fixed smoothness during rendering

--- main.cpp
+++ main.cpp
@@ -11,7 +11,6 @@ int main()
     RenderWindow window(vm,"Game Boilerplate");
     window.setFramerateLimit(60);
 
-
     Texture textureBackground;
     textureBackground.loadFromFile("assets/purple.png");
     textureBackground.setRepeated(true);
@@ -26,26 +25,59 @@ int main()
     Sprite playerShip;
     playerShip.setTexture(textureShip);
     playerShip.setPosition(1920 / 2.0f , 900);
-    int playerShipX = playerShip.getPosition().x;
-    int playerShipY = playerShip.getPosition().y;
-    float playerSpeed = 1.5;
+    float playerSpeed = 2;
+
+    bool leftArrowPressed = false, rightArrowPressed = false, upArrowPressed = false, downArrowPressed = false;
 
     while (window.isOpen())
     {
-        if (Keyboard::isKeyPressed(Keyboard::Escape))
+        /* USER INPUT */
+        Event event;
+
+        while(window.pollEvent(event))
         {
-            window.close();     
+            if (event.type == Event::KeyPressed)
+            {
+                switch(event.key.code)
+                {
+                    case Keyboard::Escape:
+                    window.close();
+                    break;
+
+                    case Keyboard::Left:
+                    leftArrowPressed = true;
+                    break;
+
+                    case Keyboard::Right:
+                    rightArrowPressed = true;
+                    break;
+                }
+            }
+            
+            if(event.type == Event::KeyReleased)
+            {
+                switch(event.key.code)
+                {
+                    case Keyboard::Left:
+                    leftArrowPressed = false;
+                    break;
+
+                    case Keyboard::Right:
+                    rightArrowPressed = false;
+                    break;
+                }
+            }
         }
-        if (Keyboard::isKeyPressed(Keyboard::Left))
+
+        if(leftArrowPressed)
         {
-            playerShipX += -1 * playerSpeed;
-            playerShip.setPosition(playerShipX, playerShipY);
+            playerShip.move(Vector2f(-playerSpeed, 0));
         }
-        if (Keyboard::isKeyPressed(Keyboard::Right))
+        else if(rightArrowPressed)
         {
-            playerShipX += 1 * playerSpeed;
-            playerShip.setPosition(playerShipX, playerShipY);
+            playerShip.move(Vector2f(playerSpeed, 0));
         }
+
 
         window.clear();
 
