4aa7acca4446c0031c796858bb7c2259b9e2c2f4
Ian Robbins <iedwardrobbins@gmail.com>
2017-06-03 23:10:51 -0500

    bullets now appear but don't move

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <vector>
+#include <time.h>
 #include "Player.hpp"
 
 using namespace sf;
@@ -10,10 +11,7 @@ struct Bullet {
     int x, y, dy;
 };
 
-std::vector<Bullet> bullets;
-
-int main()
-{
+int main() {
     VideoMode vm(600,800);
     RenderWindow window(vm,"Game Boilerplate");
     window.setFramerateLimit(60);
@@ -30,9 +28,16 @@ int main()
     Player player(t3, 300, 700, 5);
 
     int player_bullet_speed = 10;
-    Event event;
-    while (window.isOpen())
-    {
+    float player_bullet_timer = 0.0f, delay = 1.0f, time = 0.0f;
+    Clock clock;
+    std::vector<Bullet> bullets;
+
+    while (window.isOpen()) {
+        time = clock.getElapsedTime().asSeconds();
+        clock.restart();
+        player_bullet_timer += time;
+
+        Event event;
         while(window.pollEvent(event))
             if (event.type == Event::Closed)
                 window.close();
@@ -43,13 +48,14 @@ int main()
         if(Keyboard::isKeyPressed(Keyboard::Down))  player.y += player.speed;
         player.handleSprite();
 
-        if(Keyboard::isKeyPressed(Keyboard::Space))
-        {
+        if(Keyboard::isKeyPressed(Keyboard::Space) && delay < player_bullet_timer) {
             int x = player.x, y = player.y;
             Bullet bullet;
             bullet.x = x; bullet.y = y, bullet.dy = player_bullet_speed;
+            std::cout << "PLAYER Y: " << player.y << "\t" << "BULLET Y: " << bullet.y << std::endl;
             bullets.push_back(bullet);
-            std::cout << "BULLET CREATED Y POSITION: " << bullets[0].y << std::endl;
+            player_bullet_timer = 0;
+            std::cout << "BULLET CREATED Y POSITION: " << bullets[bullets.size() - 1].y << "\tBULLETS SIZE: " << bullets.size() << std::endl;
         }
 
         player.sprite.setPosition(player.x, player.y);
@@ -57,7 +63,7 @@ int main()
         window.draw(sBackground);
         window.draw(player.sprite);
 
-        for(unsigned i = 0; i > bullets.size(); i++) {
+        for(std::vector<Bullet>::size_type i = 0; i != bullets.size(); i++) {
             sBullet.setPosition(bullets[i].x, bullets[i].y - bullets[i].dy);
             window.draw(sBullet);
             std::cout << "BULLET POS Y: " << bullets[i].y << std::endl;
