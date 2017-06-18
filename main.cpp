0d1a7bead227f3b315686a8d3b7eafa908689b95
willcodes <difruscio.dev@gmail.com>
2017-06-18 13:55:10 -0400

    scrolling background and fixed foreground

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ struct Bullet {
 };
 
 struct Asteroid {
-    int x,y,dx;
+    int x,y,dx,dy;
 };
 
 int main() {
@@ -22,27 +22,22 @@ int main() {
     RenderWindow window(vm,"Space Shooter");
     window.setFramerateLimit(60);
 
-    Texture t2, t3, t4, bg1, bg2, bg3;
+    Texture t2, t3, t4, bg1, bg2;
     t2.loadFromFile("assets/bullet.png");
     t3.loadFromFile("assets/player.png");
     t4.loadFromFile("assets/asteroid_1.png");
-    bg1.loadFromFile("assets/starlayer1.png");
-    bg2.loadFromFile("assets/starlayer2.png");
-    bg3.loadFromFile("assets/starlayer3.png");
-
-    bg1.setRepeated(true);
-    bg2.setRepeated(true);
-    bg3.setRepeated(true);
+    bg1.loadFromFile("assets/background-stars.png");
+    bg2.loadFromFile("assets/background-nebula.png");
+    // bg3.loadFromFile("assets/starlayer3.png");
     
-    Sprite sBullet(t2), sAsteroid(t4), sBg1(bg1), sBg1_copy(bg1), sBg2(bg2), sBg2_copy(bg2), sBg3(bg3);
+    Sprite sBullet(t2), sAsteroid(t4), sBg1(bg1), sBg2(bg2), sBg2_copy(bg2);
     Player player(t3, 0, gWindow_h / 2, 5);
     FloatRect sBulletBounds = sBullet.getLocalBounds();
     FloatRect sAsteroidBounds = sAsteroid.getLocalBounds();
 
     int player_bullet_speed = 10;
     float player_bullet_timer = 0.0f, delay = 0.2f, time = 0.0f, asteroid_timer = 0.0f, asteroid_delay = 1.0f;
-    int sBg1_x = 0, sBg2_x = 0, sBg3_x = 0;
-    float sBg1_dx = 0.85f, sBg2_dx = 0.99f, sBg3_dx = 1.6f;
+    float sBg2_dx = 0.1f;
     Clock clock;
     std::srand ((unsigned)std::time(NULL));
     std::vector<Bullet> bullets;
@@ -76,32 +71,25 @@ int main() {
             std::cout << "BULLET CREATED Y POSITION: " << bullets[bullets.size() - 1].y << "\tBULLETS SIZE: " << bullets.size() << std::endl;
         }
         if(std::rand() % 10 + 1 > 3 && asteroid_delay < asteroid_timer) {
-            // std::srand((int)std::time(0) * gWindow_w);
-            int x = std::rand() % gWindow_w, y = 0;
+            int x = gWindow_w, y = std::rand() % gWindow_h;
             Asteroid asteroid;
-            asteroid.x = x; asteroid.y = y; asteroid.dx = 1;
+            asteroid.x = x; asteroid.y = y; asteroid.dx = rand() % 8 - 4; asteroid.dy = rand() % 8 - 4; 
             asteroids.push_back(asteroid);
             asteroid_timer = 0;
         }
         
         window.clear();
 
-        sBg1_x -= sBg1_dx * 2.5;
-        sBg2_x -= sBg2_dx * 2.6;
         
-        if(sBg1_x < -sBg1.getLocalBounds().width) sBg1_x = 0;
-        if(sBg2_x < -sBg2.getLocalBounds().width) sBg2_x = 0;
+        if(sBg2.getPosition().x < -sBg2.getLocalBounds().width) sBg2.setPosition(0, 0);
+        if(sBg2_copy.getPosition().x < 0) sBg2_copy.setPosition(sBg2.getLocalBounds().width, 0);
 
-        sBg1.setPosition(sBg1_x, 0);
-        sBg1_copy.setPosition(sBg1_x + sBg1.getLocalBounds().width, 0);
-        sBg2_copy.setPosition(sBg2_x + sBg2.getLocalBounds().width, 0);
-        sBg2.setPosition(sBg2_x, 0);
+        sBg2.move(-sBg2_dx, 0);
+        sBg2_copy.move(-sBg2_dx, 0);
 
         window.draw(sBg1);
-        window.draw(sBg1_copy);
         window.draw(sBg2);
         window.draw(sBg2_copy);
-        window.draw(sBg3);
 
         player.sprite.setPosition(player.x, player.y);
         window.draw(player.sprite);
@@ -138,7 +126,8 @@ int main() {
                 break;
             } 
             else { 
-                asteroids[i].y += asteroids[i].dx; 
+                asteroids[i].y += asteroids[i].dy;
+                asteroids[i].x += asteroids[i].dx; 
             }
                 if(asteroids[i].x > player.x && asteroids[i].x < player.x + player.w
                     && asteroids[i].y > player.y && asteroids[i].y < player.y + player.h) {
